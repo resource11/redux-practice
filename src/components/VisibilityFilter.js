511e057695db71f20b12f229113602a35d628adf
Marcela Rodriguez <arilaen@gmail.com>
2017-11-10 14:07:45 -0500

    Visibility filter updates

--- src/components/VisibilityFilter.js
+++ src/components/VisibilityFilter.js
@@ -2,7 +2,7 @@ import React from 'react';
 import FilterLink from '../containers/FilterLink';
 
 const Filter = () => (
-  <div class="filters">
+  <div className="filters">
     <FilterLink filter={0} name="Feed" />
     <FilterLink filter={1} name="My photos" />
     <FilterLink filter={2} name="Favorites" />
--- src/containers/FilterLink.js
+++ src/containers/FilterLink.js
@@ -2,9 +2,22 @@ import { connect } from 'react-redux';
 import { setVisibilityFilter } from '../actions';
 import Link from '../components/Link';
 
+const getLinkContent = filter => {
+  switch(filter) {
+    case 1:
+      return 'My Photos';
+    case 2:
+      return 'Favorites';
+    case 0:
+    default:
+      return 'Feed';
+  }
+}
+
 const mapStateToProps = (state, ownProps) => {
   return {
-    active: ownProps.filter === state.visibilityFilter
+    active: ownProps.filter === state.visibilityFilter,
+    children: getLinkContent(ownProps.filter)
   }
 };
 
